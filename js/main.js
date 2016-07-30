9bf1f962483906323f9f14b1e2fd64229fe7035e
slackpulse <slackpulse@gmail.com>
2016-07-30 09:58:46 +0900

    数値にカンマを入れる

--- js/main.js
+++ js/main.js
@@ -1,111 +1,125 @@
-jQuery(document).ready(function () {
-    var app = new Vue({
-        el: '#app',
-        data: function () {
-            return {
-                headPortable: 1000,
-                torsoPortable: 880,
-                pantsPortable: 871,
-                weaponPortable: 520,
-                additionalRate: 5,
-                histories: [],
-            };
-        },
-        ready: function () {
-            this.loadHistories();
-        },
-        computed: {
-            sum: function () {
-                return parseInt(this.headPortable, 10) + parseInt(this.torsoPortable, 10) + parseInt(this.pantsPortable, 10) + parseInt(this.weaponPortable, 10);
-            },
-            pantsAddition: function () {
-                return parseInt(this.pantsPortable) * parseInt(this.additionalRate, 10) / 100;
-            },
-            pantsAdditionWithoutOnesPlace: function () {
-                return Math.floor(this.pantsAddition / 10) * 10;
-            },
-            pantsAdditionOnesPlace: function () {
-                return this.pantsAddition % 10;
-            },
-            weaponAddition: function () {
-                return parseInt(this.weaponPortable, 10) * parseInt(this.additionalRate, 10) / 100;
-            },
-            weaponAdditionWithoutOnesPlace: function () {
-                return Math.floor(this.weaponAddition / 10) * 10;
-            },
-            weaponAdditionOnesPlace: function () {
-                return this.weaponAddition % 10;
-            },
-            isValid: function () {
-                return this.headPortable && this.torsoPortable && this.pantsPortable && this.weaponPortable;
-            },
-            portable: function () {
-                if (!this.isValid) {
-                    return '計算不可';
-                }
+String.prototype.reverse = function () {
+    return this.split('').reverse().join('');
+};
 
-                var result = this.sum + this.pantsAdditionWithoutOnesPlace + this.weaponAdditionWithoutOnesPlace;
+Number.prototype.commify = function () {
+    var str = this.toString();
+    str = str.reverse();
+    str = str.replace(/(\d\d\d)(?=\d)(?!\d*\.)/g, '$1,');
+    str = str.reverse();
+    return str;
+};
 
-                console.log('sum', this.sum);
-                console.log('pantsAdditionWithoutOnesPlace', this.pantsAdditionWithoutOnesPlace);
-                console.log('weaponAdditionWithoutOnesPlace', this.weaponAdditionWithoutOnesPlace);
-                console.log('pantsAdditionOnesPlace', this.pantsAdditionOnesPlace);
-                console.log('weaponAdditionOnesPlace', this.weaponAdditionOnesPlace);
+Vue.filter('commify', function (value) {
+    return value.commify();
+});
 
-                if (5 <= this.pantsAdditionOnesPlace) {
-                    result += this.pantsAdditionOnesPlace;
-                }
-                if (5 <= this.weaponAdditionOnesPlace) {
-                    result += this.weaponAdditionOnesPlace;
-                }
-                return Math.floor(result);
-            },
-            skating: function () {
-                if (this.additionalRate && 0 < parseInt(this.additionalRate, 10)) {
-                    return 3315 <= this.portable;
-                }
-                return 3311 <= this.portable;
-            },
+var app = new Vue({
+    el: '#app',
+    data: function () {
+        return {
+            headPortable: 1000,
+            torsoPortable: 880,
+            pantsPortable: 871,
+            weaponPortable: 520,
+            additionalRate: 5,
+            histories: [],
+        };
+    },
+    ready: function () {
+        this.loadHistories();
+    },
+    computed: {
+        sum: function () {
+            return parseInt(this.headPortable, 10) + parseInt(this.torsoPortable, 10) + parseInt(this.pantsPortable, 10) + parseInt(this.weaponPortable, 10);
         },
-        methods: {
-            loadHistories: function () {
-                var stored = window.localStorage.getItem('histories');
-                if (!stored) {
-                    this.histories = [];
-                } else {
-                    this.histories = JSON.parse(stored);
-                }
+        pantsAddition: function () {
+            return parseInt(this.pantsPortable) * parseInt(this.additionalRate, 10) / 100;
+        },
+        pantsAdditionWithoutOnesPlace: function () {
+            return Math.floor(this.pantsAddition / 10) * 10;
+        },
+        pantsAdditionOnesPlace: function () {
+            return this.pantsAddition % 10;
+        },
+        weaponAddition: function () {
+            return parseInt(this.weaponPortable, 10) * parseInt(this.additionalRate, 10) / 100;
+        },
+        weaponAdditionWithoutOnesPlace: function () {
+            return Math.floor(this.weaponAddition / 10) * 10;
+        },
+        weaponAdditionOnesPlace: function () {
+            return this.weaponAddition % 10;
+        },
+        isValid: function () {
+            return this.headPortable && this.torsoPortable && this.pantsPortable && this.weaponPortable;
+        },
+        portable: function () {
+            if (!this.isValid) {
+                return '計算不可';
+            }
+
+            var result = this.sum + this.pantsAdditionWithoutOnesPlace + this.weaponAdditionWithoutOnesPlace;
+
+            console.log('sum', this.sum);
+            console.log('pantsAdditionWithoutOnesPlace', this.pantsAdditionWithoutOnesPlace);
+            console.log('weaponAdditionWithoutOnesPlace', this.weaponAdditionWithoutOnesPlace);
+            console.log('pantsAdditionOnesPlace', this.pantsAdditionOnesPlace);
+            console.log('weaponAdditionOnesPlace', this.weaponAdditionOnesPlace);
+
+            if (5 <= this.pantsAdditionOnesPlace) {
+                result += this.pantsAdditionOnesPlace;
+            }
+            if (5 <= this.weaponAdditionOnesPlace) {
+                result += this.weaponAdditionOnesPlace;
+            }
+            return Math.floor(result);
+        },
+        skating: function () {
+            if (this.additionalRate && 0 < parseInt(this.additionalRate, 10)) {
+                return 3315 <= this.portable;
+            }
+            return 3311 <= this.portable;
+        },
+    },
+    methods: {
+        loadHistories: function () {
+            var stored = window.localStorage.getItem('histories');
+            if (!stored) {
+                this.histories = [];
+            } else {
+                this.histories = JSON.parse(stored);
+            }
+            return;
+        },
+        clear: function () {
+            this.histories.splice(0, this.histories.length);
+            window.localStorage.clear();
+        },
+        save: function () {
+            if (!this.isValid) {
                 return;
-            },
-            clear: function () {
-                this.histories.splice(0, this.histories.length);
-                window.localStorage.clear();
-            },
-            save: function () {
-                if (!this.isValid) {
-                    return;
-                }
-                this.histories.push({
-                    headPortable: this.headPortable,
-                    torsoPortable: this.torsoPortable,
-                    pantsPortable: this.pantsPortable,
-                    weaponPortable: this.weaponPortable,
-                    additionalRate: this.additionalRate,
-                    sum: this.sum,
-                    portable: this.portable,
-                    skating: this.skating,
-                });
-                this.histories = _.uniqBy(this.histories, function (record) {
-                    var key = record.headPortable + '';
-                    key += record.torsoPortable;
-                    key += record.pantsPortable;
-                    key += record.weaponPortable;
-                    key += record.additionalRate;
-                    console.log(key);
-                    return key;
-                });
-                window.localStorage.setItem('histories', JSON.stringify(this.histories));
-            },
+            }
+            this.histories.push({
+                headPortable: this.headPortable,
+                torsoPortable: this.torsoPortable,
+                pantsPortable: this.pantsPortable,
+                weaponPortable: this.weaponPortable,
+                additionalRate: this.additionalRate,
+                sum: this.sum,
+                portable: this.portable,
+                skating: this.skating,
+            });
+            this.histories = _.uniqBy(this.histories, function (record) {
+                var key = record.headPortable + '';
+                key += record.torsoPortable;
+                key += record.pantsPortable;
+                key += record.weaponPortable;
+                key += record.additionalRate;
+                console.log(key);
+                return key;
+            });
+            window.localStorage.setItem('histories', JSON.stringify(this.histories));
         },
-    });
+    },
 });
