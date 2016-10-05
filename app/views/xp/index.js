7d006323152cbd1b7b14bec0229d8f207ce8aa41
slackpulse <nomuraayu@gmail.com>
2016-10-05 20:51:13 +0900

    :+1: 経験値テーブルを正確なものに修正

--- app/views/xp/index.js
+++ app/views/xp/index.js
@@ -1,49 +1,66 @@
 var _ = require('lodash');
 
-var EXP_PER_WAPON = 350;
+var DRAW_COST = 4000;
+var XP_PER_WEAPON_1 = 350;
+var XP_PER_WEAPON_2 = 800;
+var XP_PER_WEAPON_3 = 1750;
+var FUSION_COST_PER_WEAPON_1 = 500;
+var FUSION_COST_PER_WEAPON_2 = 800;
+var FUSION_COST_PER_WEAPON_3 = 1300;
+var PROBABILITY_DRAW_RANK_1 = 82.25;
+var PROBABILITY_DRAW_RANK_2 = 12.5;
+var PROBABILITY_DRAW_RANK_3 = 5.25;
 
-var REQUIRED_RANK_ONE = [
+var REQUIRED_XP = [
     0,
-    2,
-    3,
-    4,
-    6,
-    9,
-    12,
-    15,
-    20,
-    26,
-    33,
-    42,
-    53,
-    66,
-    82,
-    101,
-    123,
-    150,
-    181,
-    218,
-    262,
-    313,
-    372,
-    440,
-    520,
-    614,
-    722,
-    846,
-    988,
-    1154,
-    1345,
-    1564,
-    1813,
-    2099,
-    2428,
-    2806,
-    3235,
-    3724,
-    4281,
-    4913,
+    379,
+    834,
+    1385,
+    2057,
+    2884,
+    3910,
+    5192,
+    6808,
+    8861,
+    11489,
+    14625,
+    18388,
+    22929,
+    28439,
+    35162,
+    43028,
+    52270,
+    63176,
+    76100,
+    91480,
+    109259,
+    129883,
+    153889,
+    181928,
+    214790,
+    252472,
+    295807,
+    345787,
+    403597,
+    470657,
+    547105,
+    634474,
+    734575,
+    849548,
+    981931,
+    1132186,
+    1303326,
+    1498169,
+    1719315,
 ];
+
+var p1 = (PROBABILITY_DRAW_RANK_1 / 100);
+var p2 = (PROBABILITY_DRAW_RANK_2 / 100);
+var p3 = (PROBABILITY_DRAW_RANK_3 / 100);
+
+var expXp1 = p1 * XP_PER_WEAPON_1;
+var expXp2 = p2 * XP_PER_WEAPON_2;
+var expXp3 = p3 * XP_PER_WEAPON_3;
 
 module.exports = {
     template: require('./template.html'),
@@ -52,30 +69,70 @@ module.exports = {
             money: 1000000,
             currentLevel: 1,
             targetLevel: 30,
+            mixRank: '3',
         };
     },
-    ready: function () {},
     computed: {
-        weaponCount: function () {
-            return parseInt(this.money / 4500, 10);
+        expectedXp: function () {
+            var result = 0;
+            if (this.mixRank === '1') {
+                result = expXp1 / (DRAW_COST + FUSION_COST_PER_WEAPON_1);
+            }
+            if (this.mixRank === '2') {
+                result = expXp1 / (DRAW_COST + FUSION_COST_PER_WEAPON_1) +
+                    expXp2 / (DRAW_COST + FUSION_COST_PER_WEAPON_2);
+            }
+            if (this.mixRank === '3') {
+                result = expXp1 / (DRAW_COST + FUSION_COST_PER_WEAPON_1) +
+                    expXp2 / (DRAW_COST + FUSION_COST_PER_WEAPON_2) +
+                    expXp3 / (DRAW_COST + FUSION_COST_PER_WEAPON_3);
+            }
+            return parseInt(result * this.money, 10);
+        },
+        expectedMoney: function () {
+            var result = 0;
+            if (this.mixRank === '1') {
+                result = (p1 * (DRAW_COST + FUSION_COST_PER_WEAPON_1)) / (p1 * XP_PER_WEAPON_1);
+            }
+            if (this.mixRank === '2') {
+                result = (p1 * (DRAW_COST + FUSION_COST_PER_WEAPON_1) +
+                    p2 * (DRAW_COST + FUSION_COST_PER_WEAPON_2)) /
+                    (p1 * XP_PER_WEAPON_1 + p2 * XP_PER_WEAPON_2);
+            }
+            if (this.mixRank === '3') {
+                result = (p1 * (DRAW_COST + FUSION_COST_PER_WEAPON_1) +
+                    p2 * (DRAW_COST + FUSION_COST_PER_WEAPON_2) +
+                    p3 * (DRAW_COST + FUSION_COST_PER_WEAPON_3)) /
+                    (p1 * XP_PER_WEAPON_1 + p2 * XP_PER_WEAPON_2 + p3 * XP_PER_WEAPON_3);
+            }
+            console.log('expectedMoney', result);
+            return result;
         },
         levelResult: function () {
             var that = this;
-            var currentValue = REQUIRED_RANK_ONE[this.currentLevel - 1];
-            var foundIndex = _.findIndex(REQUIRED_RANK_ONE, function (value, index) {
-                return that.weaponCount < value - currentValue;
+            var currentValue = REQUIRED_XP[this.currentLevel - 1];
+            var foundIndex = _.findIndex(REQUIRED_XP, function (value, index) {
+                return that.expectedXp <= value - currentValue;
             });
-
             if (foundIndex && 0 < foundIndex) {
                 return foundIndex;
+            }
+            if (that.expectedXp <= _.last(REQUIRED_XP)) {
+                return this.currentLevel;
             }
             return 40;
         },
         moneyResult: function () {
-            var that = this;
-            var targetValue = REQUIRED_RANK_ONE[this.targetLevel - 1];
-            var currentValue = REQUIRED_RANK_ONE[this.currentLevel - 1];
-            return parseInt((targetValue - currentValue) * 4500, 10);
+            var targetValue = REQUIRED_XP[this.targetLevel - 1];
+            var currentValue = REQUIRED_XP[this.currentLevel - 1];
+
+            if (parseInt(this.targetLevel, 10) < parseInt(this.currentLevel, 10)) {
+                return 0;
+            }
+            if (40 < parseInt(this.targetLevel, 10)) {
+                return 0;
+            }
+            return Math.floor((targetValue - currentValue) * this.expectedMoney);
         },
     },
 };
