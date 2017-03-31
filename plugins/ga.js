adcc9ccdb73764905d62c2ad71364108dd4a0798
slackpulse <slackpulse@gmail.com>
2017-04-01 01:12:05 +0900

    analytics修正

--- plugins/ga.js
+++ plugins/ga.js
@@ -1,17 +1,17 @@
 import router from '~router'
 if (process.env.NODE_ENV === 'production') {
-  ((i, s, o, g, r, a, m) => {
+  (function (i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r
-    i[r] = i[r] || (() => {
+    i[r] = i[r] || function () {
       (i[r].q = i[r].q || []).push(arguments)
-    })
-    i[r].l = 1 * new Date()
-    a = s.createElement(o)
+    }, i[r].l = 1 * new Date()
+    a = s.createElement(o),
     m = s.getElementsByTagName(o)[0]
     a.async = 1
     a.src = g
     m.parentNode.insertBefore(a, m)
   })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga')
+
   ga('create', 'UA-83619602-1', 'auto')
 
   router.afterEach((to, from) => {
